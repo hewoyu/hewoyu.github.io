38ebb3480304c9ea6676a38acad20632e89eb11b
Haibara AI <3484717577@qq.com>
2024-05-21 14:41:47 +0800

    Site updated: 2024-05-21 14:41:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/20/问题三/","2024/05/20/hello-world/"];function toRandomPost(){
+var posts=["2024/05/20/问题三/","2024/05/20/hello-world/","2024/05/21/obsidian的使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
