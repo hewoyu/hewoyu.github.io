3fad7906668426344d6527d5a9a4bb3dbdaa29b0
Haibara AI <3484717577@qq.com>
2024-07-24 11:36:59 +0800

    Site updated: 2024-07-24 11:36:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/21/obsidian的使用/","2024/06/20/TIAI/","2024/07/24/安知鱼博客页面配置/","2024/07/24/欢迎/"];function toRandomPost(){
+var posts=["2024/05/21/obsidian的使用/","2024/07/24/安知鱼博客页面配置/","2024/06/20/马克思主义/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
