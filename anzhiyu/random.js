9506340872df2e0a897f14cc2e4e478e9bb9ab41
Haibara AI <3484717577@qq.com>
2024-07-24 11:53:13 +0800

    Site updated: 2024-07-24 11:53:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/21/obsidian的使用/","2024/07/24/安知鱼博客页面配置/","2024/06/20/马克思主义/"];function toRandomPost(){
+var posts=["2024/05/21/obsidian的使用/","2024/07/24/安知鱼博客页面配置/","2024/06/20/马克思主义/","2024/07/24/安知鱼博客文章页面配置/","2024/07/24/科目一综合素质 国家实施素质教育的基本要求/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
